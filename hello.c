7430c8f2b4ec432f294d392b4713cfd33af0e53b
baixiaoshi <baixiaoshi@shunliandongli.com>
2017-06-28 20:45:20 +0800

    Init

--- hello.c
+++ hello.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 
+int main() {
+    char* p1 = "baixiaoshi";
+    char* p2 = (char*)malloc(sizeof(char) * 30);
+    char* retstr = (char*)memcpy(p2, p1, strlen(p1));
+    printf("retstr=%s\n", retstr);
+
+    char str[] = "i love you";
+
+    printf("str=%s\n", str);
+
+    return 0;
+}
+
