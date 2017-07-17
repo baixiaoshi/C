7171dd4e2d0980d33084ae9389f88c8f5e248c15
lion <634842632@qq.com>
2017-07-17 23:23:52 +0800

    commit all

---
+++ Process/forktest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+
+    int i = 1<<3;
+
+    printf("i=%d\n", i);
+
+}
--- hello.c
+++ hello.c
@@ -1,51 +1,15 @@
-/*C语言读取配置文件的例子*/
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <errno.h>
+#include "string.h"
+#include "stdlib.h"
 
-#define KEYVALLEN 100
+int mainaa(int argc, char *argv[]) {
 
-/*删除左边的空格*/
+    const char* str = "hello";
+    const char* str2 = "world";
 
-char *l_trim(char *output_string,const char *input_string)
-{	
+    memcpy(str + strlen(str2), str2, strlen(str));
 
-	if(output_string == NULL || input_string == NULL)
-	{
-		printf("params is not null");
-		return "";
-	}
-	char *str = "baixiaoshi";
-	
-	printf("%c\n",*str);
-	printf("sizeof=%d,strlen=%d,sizeof(char)=%d\n",sizeof(*str),strlen(str),sizeof(char));
+    printf("str=%s\n", str);
 
-
-	// int i ;
-	// for(i=0;input_string[i] != '\0';i++)
-	// {
-	// 	printf("%c\n",input_string[i]);
-	// }
-
-
-	// for(NULL;*input_string != '\0' && isspace(*input_string);++input_string)
-	// {
-	// 	;
-	// }
-	// return strcpy(output_string,input_string);
-
-}
-
-int main(int argc,char *argv[])
-{	
-	char *target_str = "";
-	char *str = "  helloworld";
-
-	printf("str_len=%d\n",strlen(str));
-
-	l_trim(target_str,str);
-	
 	return 0;
 }
