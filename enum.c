3aeeb622ae249610a47d99b8807bd67e0ff74b5d
lion <634842632@qq.com>
2017-08-22 17:08:48 +0800

    enum is constant

--- enum.c
+++ enum.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-enum {
-    int a[4];
-    char name[10];    
+enum node{
+    a = 10,
+    b,
+    c
+};
+
+
+
+int main(int argc, char *argv[]) {
+    
+    enum node testnode;
+    testnode = b;
+    printf("%d\n", testnode);    
+    return 0;
 }
+
